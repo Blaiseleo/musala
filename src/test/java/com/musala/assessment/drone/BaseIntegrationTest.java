3437a6b6f0a9cc6b9ffb5aeb9c8ab9dc3b89149f
Oscar Aguh <blaiseleo@hotmail.com>
2022-02-20 21:33:20 +0100

    - Added test cases for register drone endpoint

---
+++ src/test/java/com/musala/assessment/drone/BaseIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.musala.assessment.drone;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@Slf4j
+public abstract class BaseIntegrationTest {
+
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .alwaysDo(document("{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                .build();
+    }
+
+    static <T> String asJsonString(T map) {
+        try {
+            return new ObjectMapper().writeValueAsString(map);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/musala/assessment/drone/controllerTest.java
@@ -0,0 +1,90 @@
+package com.musala.assessment.drone;
+
+import com.musala.assessment.drone.dto.request.DroneRegistrationRequest;
+import com.musala.assessment.drone.enums.Model;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class controllerTest extends BaseIntegrationTest{
+
+    @Test
+    public void registerDroneTest_success() throws Exception {
+
+        DroneRegistrationRequest registrationRequest = DroneRegistrationRequest.builder()
+                .model(Model.LightWeight)
+                .serialNumber(RandomStringUtils.randomAlphabetic(10))
+                .weightLimit(500.00)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.
+                        post("http://localhost:9090/api/v1/drone/register")
+                        .content(asJsonString(registrationRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void registerDroneTest_failOnSerialNumberAlphaNumeric() throws Exception {
+
+        DroneRegistrationRequest registrationRequest = DroneRegistrationRequest.builder()
+                .model(Model.LightWeight)
+                .serialNumber(RandomStringUtils.randomAlphanumeric(10))
+                .weightLimit(500.00)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.
+                        post("http://localhost:9090/api/v1/drone/register")
+                        .content(asJsonString(registrationRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void registerDroneTest_failOnSerialNumberGreaterThan100() throws Exception {
+
+        DroneRegistrationRequest registrationRequest = DroneRegistrationRequest.builder()
+                .model(Model.LightWeight)
+                .serialNumber(RandomStringUtils.randomAlphabetic(101))
+                .weightLimit(500.00)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.
+                        post("http://localhost:9090/api/v1/drone/register")
+                        .content(asJsonString(registrationRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void registerDroneTest_failOnWeightLimitGreaterThan500() throws Exception {
+
+        DroneRegistrationRequest registrationRequest = DroneRegistrationRequest.builder()
+                .model(Model.LightWeight)
+                .serialNumber(RandomStringUtils.randomAlphabetic(101))
+                .weightLimit(600.00)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.
+                        post("http://localhost:9090/api/v1/drone/register")
+                        .content(asJsonString(registrationRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+    }
+}
